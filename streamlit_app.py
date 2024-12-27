bb9bd2249cfe5d06e6141dbcbfbddfa284860c81
Julio Corbaz <julio.corbaz@gmail.com>
2024-12-27 11:35:19 -0300

    Pycharm

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 julio.corbaz@gmail.com 📌")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "¡Empecemos a construir! Para obtener ayuda e inspiración, dirígete a [docs.streamlit.io](https://docs.streamlit.io/)."
 )
